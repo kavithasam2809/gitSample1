08f57d68d1f18a89a8cf289903ec359c34a39152
Dell <Dell@DESKTOP-BBGHJEO>
2023-08-06 23:52:34 +0530

    Sample Committed

--- src/test/java/sample/Demo.java
+++ src/test/java/sample/Demo.java
@@ -1,5 +1,16 @@
 package sample;
 
+import org.testng.annotations.Test;
+
 public class Demo {
+	
+	@Test
+	public void sample()
+	{
+		
+		
+		System.out.println("hi this is my first commit");
+		System.out.println("hii");
+	}
 
 }
---
+++ src/test/java/sample/Sample.java
@@ -0,0 +1,12 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+	@Test
+	public void sample()
+	{
+		System.out.println("Hello this is my second commit");
+	}
+
+}
